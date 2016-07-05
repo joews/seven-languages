5f1d6f4e07a42c818e6dc466c085e87ba3f7d27f
Joe Whitfield-Seed <joe.ws@ripjar.com>
2016-07-05 21:23:36 +0100

    Prolog: simplify optimised eight queens puzzle api

--- prolog/eight-queens.pl
+++ prolog/eight-queens.pl
@@ -1,44 +1,45 @@
-% each column must be in the range 1-8
-valid_queen((Row, Col)) :-
-  member(Col, [1,2,3,4,5,6,7,8]).
-
-valid_board([]).
-valid_board([Head|Tail]) :-
-  valid_queen(Head),
+% Each column must be a number 1-8 inclusive
+valid_cols([]).
+valid_cols([Head|Tail]) :-
+  member(Head, [1,2,3,4,5,6,7,8]),
   valid_board(Tail).
 
-% Unify Cols with the list of Col values in the input data
-cols([], []).
-cols([(_, Col)|QueenTail], [Col|ColsTail]) :-
-  cols(QueenTail, ColsTail).
-
 % Uniquely number NW-SE amd SW-NE diagonals for each (Row, Col).
+% Row is represented by the index of Col in Cols.
 diagonal1([], []).
-diagonal1([(Row, Col)|QueenTail], [Diagonal|DiagonalTail]) :-
+diagonal1([Col|QueenTail], [Diagonal|DiagonalTail]) :-
+  length(QueenTail, TailLen),
+  Row is 8 - TailLen,
   Diagonal is Col - Row,
   diagonal1(QueenTail, DiagonalTail).
 
 diagonal2([], []).
-diagonal2([(Row, Col)|QueenTail], [Diagonal|DiagonalTail]) :-
+diagonal2([Col|QueenTail], [Diagonal|DiagonalTail]) :-
+  length(QueenTail, TailLen),
+  Row is 8 - TailLen,
   Diagonal is Col + Row,
   diagonal2(QueenTail, DiagonalTail).
 
 %
 % eight queens rules
 %
-eight_queens(Board) :-
-  Board = [(1, _),(2, _),(3, _),(4, _),(5, _),(6, _),(7, _),(8, _)],
-  valid_board(Board),
+eight_queens(Cols) :-
+  length(Cols, 8),
+  valid_cols(Cols),
 
-  cols(Board, Cols),
-  diagonal1(Board, Diagonal1),
-  diagonal2(Board, Diagonal2),
+  % 8 unique rows are implicitly selected (represented by the indexes of Cols),
+  %  and 8 unique cols are explicitly selected (the values of Cols)
+  %  so we only need to work out a unique representation for each diagonal.
+  diagonal1(Cols, Diagonal1),
+  diagonal2(Cols, Diagonal2),
 
   fd_all_different(Cols),
   fd_all_different(Diagonal1),
   fd_all_different(Diagonal2).
 
-% optimize by removing row unification; rows 1-8 must necessarily
-%  be populated so we can set that as an explicit input constraint.
-% the problem is now tractible!
-% eight_queens([(1, A),(2, B),(3, C),(4, D),(5, E),(6, F),(7, G),(8, H)]);
+% rows 1-8 must always be populated, so we can reduce the input
+%  to a list of unique columns. This is tractible (unlike the first approach)
+%  and has a neat API (unlike the second approach).
+% My machine takes around one minute to find all of the solutions.
+
+% eight_queens(Cols).
